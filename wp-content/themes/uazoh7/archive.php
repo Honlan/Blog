36f4814cb54f23ba899af8b25d8be05475c18277
Honlan <honlan@192.168.0.102>
2016-06-16 21:02:26 +0800

    rm featured images in archive of posts

--- wp-content/themes/uazoh7/archive.php
+++ wp-content/themes/uazoh7/archive.php
@@ -1,1 +1,9 @@
 <?php get_header(); require_once( dirname( __FILE__ ) . '/includes/tmplane-format.php' );get_footer(); ?>
+
+<!-- Modified by Honlan -->
+<style>
+article.post figure {
+	display: none;
+}
+</style>
+<!-- Modified by Honlan -->
