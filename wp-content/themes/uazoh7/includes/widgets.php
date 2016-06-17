a6275ebce02efbbc09c880aff8171e0dab2dbd69
Honlan <honlan@192.168.0.104>
2016-06-17 22:47:24 +0800

    write widget

--- wp-content/themes/uazoh7/includes/widgets.php
+++ wp-content/themes/uazoh7/includes/widgets.php
@@ -20,11 +20,10 @@ class honlan_homepage_posts extends WP_Widget {
 	}
 
 	public function widget( $args, $instance ) {
-		extract($args);
+		// extract($args);
 		echo $instance['post_type'];
 		$query = new WP_Query( array ( 'post_type' => $instance['post_type'], 'orderby' => 'ID', 'order' => 'DESC', 'posts_per_page' => 8) );
 		$number = 0;
-		echo $args['post_type'];
 		?>
 		<style>
 		.honlan-widget {
