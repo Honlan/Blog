ad45edeb4b0bb50522c7e18dbadcbe341f90a888
Honlan <honlan@192.168.0.103>
2016-06-18 23:21:47 +0800

    responsive layout

--- wp-content/themes/uazoh7/functions.php
+++ wp-content/themes/uazoh7/functions.php
@@ -411,5 +411,12 @@ function set_post_views() {
     }  
 }  
 add_action('get_header', 'set_post_views'); 
+
+// gravatar
+function get_ssl_avatar($avatar) {
+   $avatar = preg_replace('/.*\/avatar\/(.*)\?s=([\d]+)&.*/','<img src="https://secure.gravatar.com/avatar/$1?s=$2" class="avatar avatar-$2" height="$2" width="$2">',$avatar);
+   return $avatar;
+}
+add_filter('get_avatar', 'get_ssl_avatar');
 /* Modified by Honlan */
 ?>
