9fff3a5a25c01d39294e132d49ac8f1378c9bfbf
Honlan <honlan@192.168.0.104>
2016-06-18 16:34:16 +0800

    responsive layout

--- wp-content/themes/uazoh7/includes/tmplane-format.php
+++ wp-content/themes/uazoh7/includes/tmplane-format.php
@@ -6,6 +6,14 @@ article.post figure {
 article.project figure figcaption {
 	display: none !important;
 }
+@media screen and (max-width:768px){
+	#main {
+		width: 100% !important;
+	}
+	#side {
+		display: none !important;
+	}
+}
 </style>
 <!-- Modified by Honlan -->
 
@@ -19,15 +27,15 @@ if($smof_data['blog_sidebar_pos']=='0') {
 	if (have_posts()) :  while (have_posts()) : the_post(); get_template_part( 'format', get_post_format() );endwhile;endif;  uazoh_navigation();
 	echo '</div>';
 }elseif($smof_data['blog_sidebar_pos']=='2') {
-	echo '<div class="col-xs-12 col-sm-9 col-md-9 col-lg-9" id="main">';
+	echo '<div class="col-lg-9" id="main">';
 	if (have_posts()) :  while (have_posts()) : the_post(); get_template_part( 'format', get_post_format() );endwhile;endif;  uazoh_navigation();
-	echo '</div><div class="col-xs-0 col-sm-3 col-md-3 col-lg-3" id="side">';
+	echo '</div><div class="col-lg-3" id="side">';
 	dynamic_sidebar('sidebar');
 	echo '</div>';
 }elseif($smof_data['blog_sidebar_pos']=='1') {
-	echo '<div class="col-xs-0 col-sm-3 col-md-3 col-lg-3" id="side">';
+	echo '<div class="col-lg-3" id="side">';
 	dynamic_sidebar('sidebar'); 
-	echo '</div><div class="col-xs-12 col-sm-9 col-md-9 col-lg-9" id="main">';
+	echo '</div><div class="col-lg-9" id="main">';
 	if (have_posts()) :  while (have_posts()) : the_post(); get_template_part( 'format', get_post_format() );endwhile;endif;  uazoh_navigation();
 	echo '</div>';
 }} ?>
