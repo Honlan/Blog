028292f7c933bddcc24c48e67040c7d22a1935b4
Honlan <honlan@HonlandeMacBook-Air.local>
2016-06-29 11:06:57 +0800

    modify avatar

--- wp-content/themes/uazoh7/functions.php
+++ wp-content/themes/uazoh7/functions.php
@@ -414,8 +414,8 @@ add_action('get_header', 'set_post_views');
 
 // gravatar
 function get_ssl_avatar($avatar) {
-   $avatar = preg_replace('/.*\/avatar\/(.*)\?s=([\d]+)&.*/','<img src="https://secure.gravatar.com/avatar/$1?s=$2" class="avatar avatar-$2" height="$2" width="$2">',$avatar);
-   return $avatar;
+    $avatar = preg_replace('/.*\/avatar\/(.*)\?s=([\d]+)&.*/','<img src="'.wp_get_attachment_image_src(rand(790, 799)).'" class="avatar" height="40" width="40">',$avatar);
+    return $avatar;
 }
 add_filter('get_avatar', 'get_ssl_avatar');
 /* Modified by Honlan */
